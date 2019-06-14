c9c4d73592fa7b259b4664a743008e0bb7734f4e
eliz <eliz.vitebsk@gmail.com>
2019-06-14 12:56:56 -0700

    initial commit

---
+++ searching_app/searching_app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  searching_app
+//
+//  Created by Lizaveta Rudzko on 3/24/1398 AP.
+//  Copyright © 1398 Lizaveta Rudzko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
