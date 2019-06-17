a3f101f18cc3bd3fc297ab5a89f19f7646da4342
eliz <eliz.vitebsk@gmail.com>
2019-06-17 01:58:43 -0700

    some tests added

--- searching_app/searching_appTests/searching_appTests.swift
+++ searching_app/searching_appTests/searching_appTests.swift
@@ -7,6 +7,7 @@
 //
 
 import XCTest
+import CoreData
 @testable import searching_app
 
 class searching_appTests: XCTestCase {
@@ -17,6 +18,47 @@ class searching_appTests: XCTestCase {
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    func testForFilm()
+    {
+        let film = Film(title: "title", year: "2005", image: "image", descript: "description", score: 10.0, amount: 0)
+        XCTAssertNotNil(film.title)
+        XCTAssertNotNil(film.year)
+        XCTAssertNotNil(film.image)
+        XCTAssertNotNil(film.descript)
+        XCTAssertNotNil(film.score)
+        XCTAssertNotNil(film.amount)
+    }
+    
+    func testFilmEntity()
+    {
+        let title: String = "title1"
+        let year: String = "2005"
+        let image: String = "image"
+        let descript: String = "description"
+        let score = 10.0
+        let amount = 0
+        let ap = AppDelegate.init()
+        let persistentContainer = ap.persistentContainer
+        let testObject = FilmEntity(persistentContainer: persistentContainer, title: title, year: year, image: image, descript: descript, score: score, amount: amount)
+        
+        XCTAssert(testObject.title()! == title)
+        XCTAssert(testObject.year()! == year)
+        XCTAssert(testObject.image()! == image)
+        XCTAssert(testObject.descript()! == descript)
+        XCTAssert(testObject.score()! == score)
+        XCTAssert(testObject.amount()! == amount)
+        
+        testObject.delete(persistentContainer: persistentContainer)
+        
+        XCTAssert(testObject.title() == nil)
+        XCTAssert(testObject.year() == nil)
+        XCTAssert(testObject.image() == nil)
+        XCTAssert(testObject.descript() == nil)
+        XCTAssert(testObject.score() == nil)
+        XCTAssert(testObject.amount() == nil)
+        
     }
 
     func testExample() {
